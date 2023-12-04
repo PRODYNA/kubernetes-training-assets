07240caef73526f5f70ed28f46f454accd5ec69d
Matthias Hauber <55030956+mahauber@users.noreply.github.com>
2023-12-04 13:17:27 +0100

    Create tomcat_get.js

---
+++ tomcat_get.js
@@ -0,0 +1,11 @@
+import http from 'k6/http';
+
+export let options = {
+    duration: "5m",
+    vus: 50,
+    noConnectionReuse: true
+};
+
+export default function () {
+    http.get("http://tomcat-svc/tomcat-sample/load.jsp");
+};
